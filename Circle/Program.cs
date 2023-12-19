dcf9b579feb1dc7d6bb5017fb02326ee3ef707f5
M A PROGRAMMER <M A PROGRAMMER@DESKTOP-LLLBVO5>
2023-12-19 15:14:53 +0330

    Add project files.

---
+++ Circle/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Circle
+{
+    public class Circle
+    {
+        double PI = 3.14;
+        private double radius;
+
+        public double Radius
+        {
+            get { return radius; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    radius = value;
+                }
+            }
+        }
+
+        public Circle() { }
+
+        public Circle(double Radius)
+        {
+            radius = Radius;
+        }
+
+        public double S()
+        {
+            double result = PI * radius * radius;
+            return result;
+        }
+
+        public double A()
+        {
+            double Result = 2 * PI * radius;
+            return Result;
+        }
+    }
+    class main
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter Radius :");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Circle circle = new Circle(r);
+            double a = circle.A();
+            double s = circle.S();
+            Console.WriteLine("Mohit : {0}", a);
+            Console.WriteLine("Masahat : {0}", s);
+            Console.ReadKey();
+
+        }
+    }
+}
+
